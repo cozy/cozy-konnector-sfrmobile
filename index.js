88b8040670229ee644c0568011536e77b09b0ee0
doubleface <christophe@thiriot.xyz>
2017-10-09 11:28:05 +0200

    fix: avoid MaxListenersExceededWarning error

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ function parsePage ($) {
 
   return getMoreBills()
   .then(() => {
-    return bluebird.map(trs, tr => {
+    return bluebird.mapSeries(trs, tr => {
       let link = $(tr).find('td').eq(1).find('a')
       if (link.length === 1) {
         link = baseURL + link.attr('href')
