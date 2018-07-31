a66345790fe175695b353f306540e5f4a61bba23
doubleface <christophe@cozycloud.cc>
2018-07-31 18:38:43 +0200

    chore: packaging

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 
+const entry = require('./package.json').main
+
 module.exports = {
-  entry: './index.js',
+  entry,
   target: 'node',
+  mode: 'none',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'index.js'
@@ -12,8 +15,8 @@ module.exports = {
     new CopyPlugin([
       { from: 'manifest.konnector' },
       { from: 'package.json' },
-      { from: 'yarn.lock' },
       { from: 'README.md' },
+      { from: '.travis.yml' },
       { from: 'LICENSE' }
     ])
   ]
